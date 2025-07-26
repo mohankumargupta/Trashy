a57f02ed55ed9651b1a51296178844ee71224898
Mohan Gupta <mohangupta@live.com>
2025-07-26 14:07:57 +1000

    Reset screen now can allow reset settings

--- app/src/main/java/com/melbpc/mohankumargupta/trashy/ui/components/ResetDialog.kt
+++ app/src/main/java/com/melbpc/mohankumargupta/trashy/ui/components/ResetDialog.kt
@@ -11,13 +11,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
+import androidx.tv.material3.WideButton
 
 @Composable
 fun ResetDialog(
@@ -29,14 +34,14 @@ fun ResetDialog(
     confirmText: String = "OK",
     dismissText: String = "Cancel"
 ) {
-    Row(modifier = modifier.fillMaxSize()
+    Row(
+        modifier = modifier.fillMaxSize()
     ) {
         Box(
             modifier = modifier
                 .fillMaxHeight()
                 .weight(1.3f)
-                .padding(top = 48.dp)
-            ,
+                .padding(top = 48.dp),
         ) {
             ResetDialogLHS(
                 modifier = modifier,
@@ -68,7 +73,7 @@ fun ResetDialogLHS(modifier: Modifier = Modifier, title: String, prompt: String)
         modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
 
-    ) {
+        ) {
         Text(
             title,
             modifier = modifier
@@ -89,17 +94,47 @@ fun ResetDialogRHS(
     onConfirm: () -> Unit,
     onDismissRequest: () -> Unit
 ) {
+    val cancelFocus = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        cancelFocus.requestFocus()
+    }
+
     Column(
         modifier = modifier,
     ) {
-        Text(confirmText, style = MaterialTheme.typography.bodyLarge, color = Color.White)
+        WideButton(
+            onClick = onConfirm,
+        ) {
+            Text(
+                confirmText,
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.Green,
+            )
+        }
+
         Spacer(modifier = Modifier.size(32.dp))
-        Text(dismissText, style = MaterialTheme.typography.bodyLarge, color = Color.White)
+        WideButton(
+            modifier = Modifier
+                .focusRequester(cancelFocus),
+            onClick = onDismissRequest
+        ) {
+            Text(
+                dismissText,
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.Green,
+            )
+
+        }
     }
 }
 
 @Preview
 @Composable
 fun ResetDialogPreview(modifier: Modifier = Modifier) {
-    ResetDialog(title="Reset settings", text="Are you sure you want to reset settings?", modifier = modifier)
+    ResetDialog(
+        title = "Reset settings",
+        text = "Are you sure you want to reset settings?",
+        modifier = modifier
+    )
 }
