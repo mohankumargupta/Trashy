8ac747978fc367d9bc51f21ce1473dc24f5bab22
Mohan Gupta <mohangupta@live.com>
2025-07-27 14:30:00 +1000

    added background image

--- app/src/main/java/com/melbpc/mohankumargupta/trashy/ui/onboarding/RecyclingLidScreen.kt
+++ app/src/main/java/com/melbpc/mohankumargupta/trashy/ui/onboarding/RecyclingLidScreen.kt
@@ -2,7 +2,6 @@ package com.melbpc.mohankumargupta.trashy.ui.onboarding
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.melbpc.mohankumargupta.trashy.data.model.BinType
 import com.melbpc.mohankumargupta.trashy.data.model.ColorSwatch
 import com.melbpc.mohankumargupta.trashy.ui.components.BinColor
