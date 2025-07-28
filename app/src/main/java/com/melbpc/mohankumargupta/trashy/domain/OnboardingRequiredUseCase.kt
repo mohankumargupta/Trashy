5b0a598aedba57370c5c40318dd3199bec34cb78
Mohan Gupta <mohangupta@live.com>
2025-07-28 19:10:10 +1000

    untested first usecase

--- app/src/main/java/com/melbpc/mohankumargupta/trashy/domain/OnboardingRequiredUseCase.kt
+++ app/src/main/java/com/melbpc/mohankumargupta/trashy/domain/OnboardingRequiredUseCase.kt
@@ -1,14 +1,18 @@
 package com.melbpc.mohankumargupta.trashy.domain
 
 import com.melbpc.mohankumargupta.trashy.data.repository.SettingsRepositoryInterface
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
+import javax.inject.Inject
 
-class OnboardingRequiredUseCase(
+class OnboardingRequiredUseCase @Inject constructor(
     val settingsRepository: SettingsRepositoryInterface
 ) {
+    @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke(): Flow<Boolean> {
-        return settingsRepository.load().map { collectionInfo ->
+        return settingsRepository.load().mapLatest { collectionInfo ->
             settingsRepository.isOnboardingComplete()
         }
     }
