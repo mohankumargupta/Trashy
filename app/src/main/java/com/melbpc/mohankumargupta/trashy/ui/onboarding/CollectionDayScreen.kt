3f03d9b23352920112598f88e6d9c9d9311ed895
Mohan Gupta <mohangupta@live.com>
2025-07-30 09:26:50 +1000

    remove warnings

--- app/src/main/java/com/melbpc/mohankumargupta/trashy/ui/onboarding/CollectionDayScreen.kt
+++ app/src/main/java/com/melbpc/mohankumargupta/trashy/ui/onboarding/CollectionDayScreen.kt
@@ -1,9 +1,6 @@
 package com.melbpc.mohankumargupta.trashy.ui.onboarding
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -23,9 +20,8 @@ fun CollectionDayScreen(
 }
 
 @Composable
-fun CollectionDayComposable(modifier: Modifier = Modifier, onDayChosen: (String) -> Unit) {
+fun CollectionDayComposable(onDayChosen: (String) -> Unit) {
     val days = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    val firstFocus = remember { FocusRequester() }
     val heading = "Welcome to Trashy"
     val blurb = """
         Welcome to Trashy, an app that helps you track your next bin 
@@ -43,57 +39,6 @@ fun CollectionDayComposable(modifier: Modifier = Modifier, onDayChosen: (String)
             onDayChosen(days[index])
         }
     )
-
-    /*
-
-    LaunchedEffect(Unit) {
-        firstFocus.requestFocus()
-    }
-
-    Row(modifier = modifier.fillMaxSize()) {
-
-
-        Box(
-            modifier = modifier
-                .fillMaxHeight()
-                .weight(2f)
-                .padding(horizontal = 16.dp), contentAlignment = Alignment.Center
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(32.dp)
-            ) {
-                Text(heading, style = MaterialTheme.typography.headlineLarge)
-                Text(blurb)
-            }
-        }
-
-
-        LazyColumn(
-            modifier = modifier
-                .fillMaxHeight()
-                .weight(1f)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-
-        ) {
-            itemsIndexed(days) { index, day ->
-                WideButton(
-                    onClick = { onDayChosen(day) },
-                    modifier = if (index == 0) {
-                        modifier.focusRequester(firstFocus)
-                    } else {
-                        modifier
-                    }
-                ) {
-                    Text(day)
-                }
-
-            }
-        }
-    }
-
-     */
 }
 
 
@@ -103,8 +48,9 @@ fun CollectionDayComposable(modifier: Modifier = Modifier, onDayChosen: (String)
     name = "720p",
     device = Devices.TV_720p,
 )
-fun CollectionDayPreview(modifier: Modifier = Modifier) {
+fun CollectionDayPreview() {
     TrashyTheme(isInDarkTheme = true) {
-        CollectionDayComposable(modifier = modifier, onDayChosen = {})
+        CollectionDayComposable(onDayChosen = {})
     }
 }
+
