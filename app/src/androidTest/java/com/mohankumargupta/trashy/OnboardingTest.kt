51b9fcde66ab1be5a58cd81a603be648c0f8f837
Mohan Gupta <mohangupta@live.com>
2025-07-31 16:33:52 +1000

    updating dependencies

--- app/src/androidTest/java/com/mohankumargupta/trashy/OnboardingTest.kt
+++ app/src/androidTest/java/com/mohankumargupta/trashy/OnboardingTest.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.melbpc.mohankumargupta.trashy.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
